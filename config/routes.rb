f86f6d59f926efa51c240e9be535358aecf3426c
ISale7 <ibrahim@zenhr.com>
2021-11-07 09:44:03 +0200

    Added bootstrap panels

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+
+  get 'test' => 'pages#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
